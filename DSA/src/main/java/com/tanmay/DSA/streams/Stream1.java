ea048ad3557d3242f37e3e24a63f42839e2ddc32
Tanmay Makode <90134325+TANMAYMAKODE@users.noreply.github.com>
2026-03-09 21:39:26 +0530

    Stream1

--- DSA/src/main/java/com/tanmay/DSA/streams/Stream1.java
+++ DSA/src/main/java/com/tanmay/DSA/streams/Stream1.java
@@ -1,4 +1,27 @@
 package com.tanmay.DSA.streams;
 
+import java.util.*;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Stream1 {
+
+    public static void main(String[] args) {
+
+
+        String input = "siewss";
+        Optional<Character> firstNonRepeated = input.chars()
+                .mapToObj(c -> (char) c)
+                .filter(c -> input.indexOf(c) == input.lastIndexOf(c))
+                .findFirst();
+        System.out.println(firstNonRepeated.orElse(null)); // Output: i
+
+
+        Optional<Character> out = input.chars()
+                .mapToObj(c -> (char) c)
+                .filter(c -> input.indexOf(c) == input.lastIndexOf(c)).sorted()
+                .findFirst();
+        System.out.println(out.orElse(null)); // Output: e
+    }
 }
